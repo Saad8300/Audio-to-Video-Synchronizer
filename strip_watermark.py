a50c8ba96ea85babb1867011183fe7118b0c1100
Saad8300 <gascahya2@gmail.com>
2026-06-28 23:25:08 +0500

    Remove temp script files

--- strip_watermark.py
+++
@@ -1,8 +0,0 @@
-import os, re
-for f in ["frontend/src/types/index.ts"]:
-    with open(f, "r") as file:
-        lines = file.readlines()
-    with open(f, "w") as file:
-        for line in lines:
-            if "watermark" not in line.lower() and "Watermark" not in line:
-                file.write(line)
